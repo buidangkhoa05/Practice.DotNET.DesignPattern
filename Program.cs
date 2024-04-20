d3977f57317d7ee880d703f3c01dcf7746cc0fb5
khoabui <buidangkhoa05@gmail.com>
2024-04-20 21:42:16 +0700

    feat : Mediator pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MediatorPattern;
+
 Console.WriteLine("Hello, World!");
+
+var mediatorDemo = new MediatorDemo();
+
+mediatorDemo.Run(); 
